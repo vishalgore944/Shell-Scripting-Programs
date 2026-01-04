bfa0b6b312198c8b84d278b061e4329c69f02c95
cloud <vishalgore944@gmail.com>
2026-01-04 17:07:22 +0530

    002 Program to ask user to enter username

---
+++ 002userdata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##
+# Author: Vishal Gore
+# Action: Ask user to add his username.
+##
+
+# Static variable
+
+course="DevOps"
+
+# Ask for inputs
+
+echo "What is your name?"
+read username
+
+echo "Hello $username, welcome to the $course practice session"
